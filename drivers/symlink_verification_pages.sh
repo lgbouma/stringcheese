fc339be5f961b37273b7411096e8f172603bc340
lgbouma <bouma.luke@gmail.com>
2019-09-02 16:21:12 -0400

    add drivers/symlink_verification_pages.sh

---
+++ drivers/symlink_verification_pages.sh
@@ -0,0 +1,20 @@
+#
+# after generating the verification pages ("reports") symlink them into a
+# group-specific subdirectory.  errors in the symlinking (e.g., if the symlink
+# already exists) are sent to /dev/null.
+#
+groupdirs=`ls -d /home/luke/stringcheese_cutouts/group*`
+
+for g in $groupdirs; do
+
+  echo 'symlinking '$g' ...'
+
+  bn=`basename $g`
+
+  outdir=/home/luke/stringcheese_cutouts/verification_pages/$bn
+
+  [ ! -d $outdir ] && mkdir $outdir
+
+  ln -s $g/*/*png $outdir/. 2>/dev/null
+
+done
